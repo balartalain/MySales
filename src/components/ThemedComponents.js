679637d5f729baa3174600ea2f697f9a7a98d859
Alain Pérez Balart <alain.perez@openbravo.com>
2024-03-06 23:08:13 -0400

    Refactored ThemedComponents

--- src/components/ThemedComponents.js
+++ src/components/ThemedComponents.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, TextInput, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import { View, TextInput, Text, StyleSheet, TouchableOpacity, Button, Pressable  } from 'react-native';
 import useTheme from '../useTheme';
 
 function withStyles(Component) {
@@ -9,35 +9,43 @@ function withStyles(Component) {
     }
 }
    
-const ThemedTouchableOpacity = ({children, ...props})=> {
+const ThemedButton = ({children, ...props})=> {
   const theme = useTheme();
   const backgroundColor =  props.bg?theme[props.bg]:undefined;
-  const styles = {
-    ...props,
-    backgroundColor   
+  const btnStyles = {
+    ...styles.button,
+    ...props.style,
+    backgroundColor
+  }
+  const btnText = {
+    ...styles.text,
+    ...props.textStyle
   }
   return (
-    <TouchableOpacity style={styles}>
-      {children}
+    <TouchableOpacity style={btnStyles}>
+      {
+      !props.title?children
+      :<Text style={btnText}>{props.title}</Text>
+      }
     </TouchableOpacity>
   )
 }
-const ThemedButton = ({children, ...props})=> {
-  console.log(props)
-  const theme = useTheme();
-  const backgroundColor =  props.bg?theme[props.bg]:undefined;
-
-  return (
-    <Button color={backgroundColor} title={props.title} style={props}>
-      {children}
-    </Button>
-  )
-}
-export {ThemedTouchableOpacity, ThemedButton};
-// const Button = (props) = {
-//     return 
-// }
-// const Text = () => <p>Hello World!</p>
-
-// const StyledButton = withStyles(Button)
-// const StyledText = withStyles(Text)
+export {ThemedButton};
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});

4632e512cacc6d11dedd392402f086eac9fcff97
Alain Pérez Balart <alain.perez@openbravo.com>
2024-03-14 01:35:17 -0400

    refactored

--- src/screens/AddProduct.js
+++ src/screens/AddProduct.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, TextInput, StyleSheet, Image, Text } from 'react-native';
+import {launchImageLibrary, launchCamera} from 'react-native-image-picker';
 import RNFS from 'react-native-fs';
 import { useProduct } from '../ProductProvider';
 import { ThemedButton } from '../components/ThemedComponents';
+import TakePhoto from '../components/TakePhoto';
 
 const AddProduct = ({navigation}) => {
     const {addProduct} = useProduct();
@@ -12,9 +14,9 @@ const AddProduct = ({navigation}) => {
       name: '',
       price: '',
     });
-
+    
     const handleInputChange = (fieldName, value) => {
-        setProduct({ ...product, [fieldName]: value });        
+      setProduct({ ...product, [fieldName]: value });        
     };
     const handleAddProduct = () => {
         addProduct(product);
@@ -25,9 +27,21 @@ const AddProduct = ({navigation}) => {
         price: '',
     });
     };
-
+    const createPictureDir = async()=>{
+      try{
+        const picturePath = `${RNFS.DocumentDirectoryPath}/images`;
+        console.log(picturePath)
+        await RNFS.mkdir(picturePath);
+          
+      }
+      catch(error){
+        console.log('Error creating directory:', error);
+      }
+    }
     useEffect(() => {
+      //createPictureDir();
       const directoryPath = RNFS.DocumentDirectoryPath ;
+
       console.log(directoryPath);
       RNFS.readdir(directoryPath)
         .then(files => {
@@ -38,6 +52,42 @@ const AddProduct = ({navigation}) => {
         });
     }, []);
 
+    const onSelectedImage = useCallback((imageUri)=>{
+      setProduct({ ...product, image: imageUri });    
+    }, [product.imageUri]);
+    
+    const moveAttachment = async (filePath, newFilepath) => {
+      return new Promise((resolve, reject) => {
+        RNFS.mkdir(dirPicutures)
+          .then(() => {
+            RNFS.moveFile(filePath, newFilepath)
+              .then(() => {
+                console.log('FILE MOVED', filePath, newFilepath);
+                resolve(true);
+              })
+              .catch(error => {
+                console.log('moveFile error', error);
+                reject(error);
+              });
+          }) 
+          .catch(err => {
+            console.log('mkdir error', err);
+            reject(err);
+          });
+      });
+    };
+    const saveImage = async filePath => {
+      try {
+        // set new image name and filepath
+        const newImageName = `${moment().format('DDMMYY_HHmmSSS')}.jpg`;
+        const newFilepath = `${dirPicutures}/${newImageName}`;
+        // move and save image to new filepath
+        const imageMoved = await moveAttachment(filePath, newFilepath);
+        console.log('image moved', imageMoved);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     return (
     <View style={styles.container}>
         <TextInput
@@ -54,11 +104,7 @@ const AddProduct = ({navigation}) => {
         keyboardType="numeric"
         />
         <View style={{flexDirection: 'row', marginBottom: 10, borderRadius:5, borderWidth:1, borderColor: '#ccc'}}>
-          <Image style={{width:100, height:100}} source={require("../../assets/images/no-image.png")}/>
-          <View style={{justifyContent: 'space-around'}}>
-            <ThemedButton style={{paddingVertical: 6}} bg={'primaryColor'} title="Elija una foto" onPress={handleAddProduct} />
-            <ThemedButton style={{paddingVertical: 6}} bg={'primaryColor'} title="Tomar una foto" onPress={handleAddProduct} />    
-          </View>
+          <TakePhoto onSelectedImage={onSelectedImage}/>
         </View>
         <ThemedButton bg={'primaryColor'} title="Agregar Producto" onPress={handleAddProduct} />
         <View>
